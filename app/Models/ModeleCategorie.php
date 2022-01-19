7e854720b8cf7eecdcd19172dfdd245e1525563c
JuDev22 <b.julian.pro@icloud.com>
2022-01-19 15:04:08 +0100

    valide

---
+++ app/Models/ModeleCategorie.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ModeleCategorie extends Model
+{
+
+    protected $table = 'categorie';
+    protected $allowedFields = ['NOCATEGORIE', 'LIBELLE'];
+    protected $primaryKeyn= 'NOCATEGORIE';
+
+    public function inserer_une_categorie($pDonneesAInserer)
+    {
+        return $this->insert($pDonneesAInserer);
+    }
+    public function retourner_categories($pNoCategorie = false)
+    {
+        if ($pNoCategorie === false) {
+            return $this->orderBy('LIBELLE', 'asc')
+            ->findAll();
+        }
+
+        return $this->where(['NOCATEGORIE' => $pNoCategorie])
+        ->first();
+    }
+}
---
+++ app/Views/AdministrateurSuper/ajouter_une_marque.php
@@ -0,0 +1,19 @@
+<div>
+    <div class="container">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-md-6">
+                <div class="col-md-12 container">
+                    <br>
+                    <h2 class="text-primary"><?php echo $TitreDeLaPage ?></h2>
+                    <?php if ($TitreDeLaPage == 'Corriger votre formulaire') echo service('validation')->listErrors();
+                    echo form_open_multipart('AdministrateurSuper/ajouter_une_marque');
+                    ?>
+                    <label class="text-primary" for="nom">Nom : </label>
+                    <input class="form-control" type="input" name="nom" value="<?php echo set_value('nom'); ?>" /><br />
+                    <input class="btn btn-primary btn-md" type="submit" name="submit" value="Ajouter une marque" />
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
